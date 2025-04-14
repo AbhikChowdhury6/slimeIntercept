adbde5b34df81e1e8061ce9cab5bbc1f9066bd28
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-04-13 17:53:24 -0700

    some prints in ctsb

--- circularTimeSeriesBuffer.py
+++ circularTimeSeriesBuffer.py
@@ -40,6 +40,8 @@ class CircularTimeSeriesBuffers:
         #print("in set item")
         #sys.stdout.flush()
         index = index % self.size[0]  # Ensure circular indexing
+        print("value[0]:", value[0])
+        print("shape of value[0]:", np.array(value[0]).shape)
         self.data_buffers[self.bn[0]][index] = torch.tensor(value[0])  # Assume value is a tuple (data, timestamp)
         self.time_buffers[self.bn[0]][index] = torch.tensor(int(value[1].replace(tzinfo=timezone.utc).timestamp() * 1e9))
             
